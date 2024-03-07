1b6d696f9424641d12e8693a6a7fa887b6ed9143
Mayank Borasi <124684749+MrMayank28@users.noreply.github.com>
2024-03-07 17:10:23 +0000

    string important concept

---
+++ Tutorial_Series/08_stringConcept.js
@@ -0,0 +1,32 @@
+const name = "Mayank"
+const repoCount = 10
+
+// console.log(name + " " + repoCount + " Value");
+
+// `` is call backticks     // String Interpolation
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('Mayank_B')
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf("k"));
+// console.log(gameName.substring(0, 3).toUpperCase());
+
+// console.log(gameName.slice(-8, 3));
+
+const newName = "       Mayank     "
+// console.log(newName);
+// console.log(newName.trim());
+
+const url = "https://mayank.com/mayank%20borasi"
+// console.log(url.replace("%20", "-"));
+// console.log(url.includes("mayank"));
+
+const dashedString = "Mayank-Borasi-Prachi-Singh"
+console.log(dashedString.split('-'));
