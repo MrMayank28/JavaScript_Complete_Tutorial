7c21c32a369874ecdd0dea0d4691bc35b94fb6e5
Mayank Borasi <124684749+MrMayank28@users.noreply.github.com>
2024-03-07 15:11:00 +0000

    operations

---
+++ Tutorial_Series/05_operations.js
@@ -0,0 +1,35 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello "
+let str2 = "Mayank"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2  // Ignore Complex/Confusing Codding
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
