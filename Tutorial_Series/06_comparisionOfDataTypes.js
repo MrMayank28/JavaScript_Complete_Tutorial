a930aa19457bd40e8d49e54115ba6c3e2e2e3272
Mayank Borasi <124684749+MrMayank28@users.noreply.github.com>
2024-03-07 15:20:57 +0000

    comparision

---
+++ Tutorial_Series/06_comparisionOfDataTypes.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);   // DataType Should Be Same
+// console.log("02" > 1);  // DataType Should Be Same
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+
+// === (Strict Check)
+
+console.log("2" == 2);  // Check The Value
+console.log("2" === 2); // Check The value And DataType As Well
